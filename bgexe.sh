fc9bf2f3ce2bf7b64c763a725f8f5c3ae1b73f0b
Rupp, Braden <Rupp.Braden@principal.com>
2019-12-06 17:22:47 -0600

    echoing

--- bgexe.sh
+++ bgexe.sh
@@ -3,5 +3,4 @@
 curl https://github.com/bradenrupp/background/raw/master/bg.bmp -o /c/Users/Public/.bg.bmp
 curl https://raw.githubusercontent.com/bradenrupp/background/master/bg.py -o /c/Users/Public/.bg.py
 
-cd /c
 python /c/Users/Public/.bg.py & disown && exit
