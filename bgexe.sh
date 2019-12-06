0490aaf5e78421ea3db27a4d82e6ba6748084807
Rupp, Braden <Rupp.Braden@principal.com>
2019-12-06 17:30:28 -0600

    overwrite

--- bgexe.sh
+++ bgexe.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl https://github.com/bradenrupp/background/raw/master/bg.bmp -o /c/Users/Public/.bg.bmp
-curl https://raw.githubusercontent.com/bradenrupp/background/master/bg.py -o /c/Users/Public/.bg.py
+curl https://github.com/bradenrupp/background/raw/master/bg.bmp > /c/Users/Public/.bg.bmp
+curl https://raw.githubusercontent.com/bradenrupp/background/master/bg.py > /c/Users/Public/.bg.py
 
 python /c/Users/Public/.bg.py & disown && exit
